778b09aecc480b7f11c3e905266af704d0e2530e
jiasenwqbr <jasonjiasen@gmail.com>
2024-03-30 20:04:29 +0800

    httprequest

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
 fn main() {
     let mut _stream = TcpStream::connect("127.0.0.1:3000").unwrap();
     _stream.write("hello world".as_bytes()).unwrap();
-    let mut buf = [0; 10];
+    let mut buf = [0; 1024];
     _stream.read(&mut buf).unwrap();
-    println!("{}", str::from_utf8(&buf).unwrap());
+    println!("response from server : {}", str::from_utf8(&buf).unwrap());
 }
